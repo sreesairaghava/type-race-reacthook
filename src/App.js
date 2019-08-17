e78470b24b2e27f6ab0899b73f1ccd249c6fbb3d
Sree Sai Raghava <raghava.rym@gmail.com>
2019-08-17 21:24:46 +0530

    Final Commit v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const SNIPPETS = [
+    "Bears, beets, battlestar galactica",
+    "Where do programmers like to hangout? The Foo Bar",
+    "What's Forrest Gump's password? Forrest1",
+    "Sree Sai Raghava"
+  ];
+  //!This is new feature to use React Hooks
+  const INITIAL_GAME_START = { victory: false, startTime: null, endTime: null };
+  const [snippet, setSnippet] = useState("");
+  const [userText, setUserText] = useState("");
+  const [gameState, setGameState] = useState(INITIAL_GAME_START);
+  //* Creating Helper Function to set user text
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  };
+  // *Creating Helper Function to choose Snippet
+  const chooseSnippet = snippetIndex => () => {
+    console.log("setSnippet", snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({
+      ...gameState,
+      startTime: new Date().getTime()
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        width: "80%",
+        height: "500px",
+        margin: "0 auto"
+      }}
+    >
+      <h2
+        style={{
+          color: "red"
+        }}
+      >
+        Type Race
+      </h2>
+      {SNIPPETS.map((SNIPPET, index) => (
+        <button
+          onClick={chooseSnippet(index)}
+          key={index}
+          style={{
+            padding: "10px",
+            color: "white",
+            borderRadius: "5px",
+            backgroundColor: "blue",
+            border: "none",
+            margin: "10px",
+            cursor: "pointer"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {SNIPPET.substring(0, 10)}...
+        </button>
+      ))}
+      <br />
+      <h4> {snippet} </h4>
+      <br />
+      <input
+        value={userText}
+        onChange={updateUserText}
+        style={{
+          padding: "20px",
+          marginTop: "20px",
+          width: "50%",
+          outline: "none",
+          color: "red"
+        }}
+      />
+      <h4>
+        {gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}
+      </h4>
+      <hr />
     </div>
   );
-}
+};
 
 export default App;
